c3c255c7921715597446bb6c0c07506f64763f4a
Sabrina <sabrina@madethebeat.com>
2026-05-11 20:23:25 +0200

    Fix Keystatic config

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -3,6 +3,7 @@ import { config, fields, collection, singleton } from '@keystatic/core';
 export default config({
   storage: {
     kind: 'github',
+    branchPrefix: 'keystatic/',
     repo: 'creativesolace/creative-solace',
   },
 
