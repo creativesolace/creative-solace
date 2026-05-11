be2c6b350802a88577751c6808d6d4359146c94a
Sabrina <sabrina@madethebeat.com>
2026-05-11 21:05:39 +0200

    Fix Keystatic config import path

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -1,4 +1,4 @@
 export const prerender = false;
 import { makeHandler } from '@keystatic/astro/api';
-import config from '../../../keystatic.config';
+import config from '../../../../keystatic.config';
 export const ALL = makeHandler({ config });
